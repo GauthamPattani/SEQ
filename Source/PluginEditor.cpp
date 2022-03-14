7504d27fc0e613a745a16cfd4459c66ca50a6136
Gautham Pattani <gautham.pattani@gmail.com>
2022-03-14 12:03:39 +0530

    Sliders Added

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,6 +15,10 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+    for(auto*comp : getComps())
+    {
+        addAndMakeVisible(comp);
+    }
     setSize (400, 300);
 }
 
@@ -30,11 +34,42 @@ void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Plugin!", getLocalBounds(), juce::Justification::topLeft, 1);
 }
 
 void SimpleEQAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    auto bounds =getLocalBounds();
+    auto responseArea = bounds.removeFromTop(bounds.getHeight()*0.33);
+    auto lowCutArea =bounds.removeFromLeft(bounds.getWidth()*0.33);
+    auto highCutArea =bounds.removeFromRight(bounds.getWidth()*0.5);
+    
+    lowCutFreqSlider.setBounds(lowCutArea.removeFromTop(lowCutArea.getHeight()*0.5));
+    lowCutSlopeSlider.setBounds(lowCutArea);
+    
+    highCutFreqSlider.setBounds(highCutArea.removeFromTop(highCutArea.getHeight()*0.5));
+    highCutSlopeSlider.setBounds(highCutArea);
+    
+    peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.33));
+    peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5));
+    peakQualitySLider.setBounds(bounds);
 }
+
+std::vector<juce::Component*>SimpleEQAudioProcessorEditor:: getComps()
+{
+    return
+    {
+        &peakFreqSlider,
+        &peakGainSlider,
+        &peakQualitySLider,
+        &lowCutFreqSlider,
+        &highCutFreqSlider,
+        &highCutSlopeSlider,
+        &lowCutSlopeSlider
+        
+    };
+}
+
