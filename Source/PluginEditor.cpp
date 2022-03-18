bc3f1297b45760ef0894d92d8e8f471a9f51b9f7
Gautham Pattani <gautham.pattani@gmail.com>
2022-03-18 15:52:49 +0530

    Added Analyser Db marks

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -400,6 +400,16 @@ void ResponseCurveComponent::resized()
         g.setColour(gDb == 0.f ? Colour(0u, 102u, 204u): Colours::lightgrey);
         
         g.drawFittedText(str, r, juce::Justification::centred,1);
+        
+        str.clear();
+        str<< (gDb- 24.f);
+        
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        
+        g.drawFittedText(str, r, juce::Justification::centred,1);
     }
 }
 
