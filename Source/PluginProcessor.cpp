e101492879b64b4496a1d1537e8e3ace72c35d76
Gautham Pattani <gautham.pattani@gmail.com>
2022-03-21 11:56:48 +0530

    Implemented basic FFT analyzer

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -105,7 +105,9 @@ void SimpleEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
     rightChain.prepare(spec);
     
     updateFilters();
-   
+    
+    leftChannelFifo.prepare(samplesPerBlock);
+    rightChannelFifo.prepare(samplesPerBlock);
     
 }
 
@@ -171,6 +173,9 @@ void SimpleEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     
     leftChain.process(leftContext);
     rightChain.process(rightContext);
+    
+    leftChannelFifo.update(buffer);
+    rightChannelFifo.update(buffer);
 
 }
 
